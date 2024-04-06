715554e6bbb13a5aee7f512705d11ac6cab1435a
Walid Ara <arawalid90@gmail.com>
2024-04-06 14:51:42 +0100

    removed client

--- client/src/components/ui/home/navbar/IsAuth.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import UserOptions from "./UserOptions";
-import LoginBtn from "./LoginBtn";
-
-export default function IsAuth() {
-  const session = false;
-
-  return <>{session ? <UserOptions /> : <LoginBtn />}</>;
-}
--- client/types-end.d.ts
+++
@@ -1,3 +0,0 @@
-type Children = {
-  children: React.ReactNode;
-};
