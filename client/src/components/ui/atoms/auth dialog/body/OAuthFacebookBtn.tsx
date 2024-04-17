9f27ba143f8cc004b184c1a2cb720a580fc2ac98
Walid Ara <arawalid90@gmail.com>
2024-04-17 17:16:02 +0100

    fixed ui

--- client/src/components/ui/atoms/auth dialog/body/OAuthFacebookBtn.tsx
+++ client/src/components/ui/atoms/auth dialog/body/OAuthFacebookBtn.tsx
@@ -1,11 +1,17 @@
+"use client";
 import { Button } from "@/components/cli/button";
+import { useTheme } from "next-themes";
 import React from "react";
 import { FaFacebook } from "react-icons/fa";
 
 export default function OAuthFacebookBtn() {
+  const { theme } = useTheme();
   return (
-    <Button className="flex justify-center items-center gap-2 w-full ">
-      <FaFacebook className="text-blue-500 shrink-0" />
+    <Button
+      variant={theme === "light" || theme === "system" ? "outline" : "default"}
+      className="flex justify-center items-center gap-2 w-full "
+    >
+      <FaFacebook className="text-blue-500 shrink-0 text-lg" />
       <span>Login with facebook</span>
     </Button>
   );
--- client/src/components/ui/atoms/auth dialog/body/OAuthGoogleBtn.tsx
+++ client/src/components/ui/atoms/auth dialog/body/OAuthGoogleBtn.tsx
@@ -1,15 +1,15 @@
 "use client";
-// File: OAuthGoogleBtn.js
-// Import necessary dependencies
+
 import { Button } from "@/components/cli/button";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { auth } from "@/firebase/firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useTheme } from "next-themes";
 
 export default function OAuthGoogleBtn() {
   const googleAuth = new GoogleAuthProvider();
-
+  const { theme } = useTheme();
   googleAuth.setCustomParameters({
     prompt: "select_account",
     login_hint: "user@example.com",
@@ -18,9 +18,6 @@ export default function OAuthGoogleBtn() {
   const handleOAuth = async () => {
     try {
       const firebaseResult = await signInWithPopup(auth, googleAuth);
-      console.log("====================================");
-      console.log(firebaseResult);
-      console.log("====================================");
     } catch (error) {
       console.error("Error signing in with Google:", error);
     }
@@ -28,10 +25,11 @@ export default function OAuthGoogleBtn() {
 
   return (
     <Button
+      variant={theme === "light" || theme === "system" ? "outline" : "default"}
       onClick={handleOAuth}
-      className="flex justify-center items-center gap-2 w-full"
+      className="flex justify-center items-center gap-2 w-full "
     >
-      <FcGoogle />
+      <FcGoogle className="text-lg" />
       <span>Login with Google</span>
     </Button>
   );
